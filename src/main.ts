e6c321aacfda79ad83e024d25c235ea73dc9ea70
siwonpada <psw041115@gmail.com>
2024-04-25 01:13:39 +0900

    add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import expressBasicAuth from 'express-basic-auth';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // swagger auth config
+  const configService = app.get(ConfigService);
+  app.use(
+    ['/api'],
+    expressBasicAuth({
+      challenge: true,
+      users: {
+        [configService.getOrThrow('SWAGGER_USER')]:
+          configService.getOrThrow('SWAGGER_PASSWORD'),
+      },
+    }),
+  );
+  // set swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Boggle API')
+    .setDescription('The Boggle API description')
+    .setVersion('2024.4')
+    .addTag('boggle')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // start server
   await app.listen(3000);
 }
 bootstrap();
