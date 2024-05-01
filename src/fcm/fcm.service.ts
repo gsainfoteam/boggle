e2fa0e2b5da14e9108fe3e209988e7d0fc55562c
siwonpada <psw041115@gmail.com>
2024-05-02 02:20:38 +0900

    modify sent fcm

--- src/fcm/fcm.service.ts
+++ src/fcm/fcm.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { Service } from '@prisma/client';
-import { App } from 'firebase-admin/app';
+import { cert, initializeApp } from 'firebase-admin/app';
 import { Notification, getMessaging } from 'firebase-admin/messaging';
 import { FcmRepository } from './fcm.repository';
 
@@ -9,18 +9,29 @@ export class FcmService {
   constructor(private readonly fcmRepository: FcmRepository) {}
 
   async postMessage(
-    app: App,
     notification: Notification,
     service: Service,
     tokens: string[],
     data?: Record<string, string>,
   ): Promise<void> {
+    // Initialize Firebase Admin SDK
+    const app = initializeApp({
+      credential: cert({
+        projectId: service.projectName,
+        clientEmail: service.email,
+        privateKey: service.privateKey,
+      }),
+    });
+
+    // send message to each token
     const result = await getMessaging(app).sendEachForMulticast({
       tokens,
       notification,
       apns: { payload: { aps: { mutableContent: true } } },
       data,
     });
+
+    // update success and fail count
     const responses = result.responses.map((res, idx) => ({
       res,
       token: tokens[idx],
@@ -35,6 +46,7 @@ export class FcmService {
       failed.map(({ token }) => token),
     );
 
+    // create logs
     await this.fcmRepository.createLogs(
       { notification, data },
       sucessed.map(({ token }) => token),
