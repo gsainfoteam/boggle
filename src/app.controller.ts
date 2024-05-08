1e7756765c485d0b12bc87c1d5e4748873c8597a
minjunj <minjun_jo@gm.gist.ac.kr>
2024-05-08 17:25:32 +0900

    create : add post service api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,17 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
+import { PostServiceDto } from './dto/app.dto';
+import { FcmService } from './fcm/fcm.service';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
+  constructor(
+    private readonly fcmService: FcmService,
+    private prisma: PrismaService,
+  ) {}
+
   @ApiResponse({
     status: 200,
     description: 'Send the message "pong" when the server is active.',
@@ -19,5 +28,18 @@ export class AppController {
   @Get()
   getHello(): string {
     return 'pong';
+  }
+
+  @Post('/service')
+  async PostService(@Body() dataDto: PostServiceDto): Promise<object> {
+    const service = await this.prisma.service.create({
+      data: {
+        name: dataDto.name,
+        privateKey: dataDto.private_key,
+        projectName: dataDto.project_name,
+        email: dataDto.email,
+      },
+    });
+    return service;
   }
 }
---
+++ src/dto/app.dto.ts
@@ -0,0 +1,9 @@
+export class PostServiceDto {
+  readonly name: string;
+
+  readonly private_key: string;
+
+  readonly project_name: string;
+
+  readonly email: string;
+}
