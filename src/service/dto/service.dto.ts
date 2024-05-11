8786d480a209cb0fa5889b76eaf36a51acfb085a
minjunj <minjun_jo@gm.gist.ac.kr>
2024-05-12 03:17:05 +0900

    update : make wagger desctiption

--- src/service/dto/service.dto.ts
+++ src/service/dto/service.dto.ts
@@ -1,17 +1,43 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class PostServiceDto {
+  @ApiProperty({
+    description: 'name',
+    default: 'name',
+  })
   readonly name: string;
 
+  @ApiProperty({
+    description: 'pricate key',
+    default: 'ASDWA223_asD',
+  })
   readonly private_key: string;
 
+  @ApiProperty({
+    description: 'project_name',
+    default: 'projectname',
+  })
   readonly project_name: string;
 
+  @ApiProperty({
+    description: 'email',
+    default: 'example@email.com',
+  })
   readonly email: string;
 }
 
 export class PushMessageDto {
   readonly notification: Notification;
 
+  @ApiProperty({
+    description: 'data',
+    default: 'I dont know yet..',
+  })
   readonly data?: Record<string, string>;
 
+  @ApiProperty({
+    description: 'forwarding',
+    default: 'I dont know yet..',
+  })
   readonly forwarding: string[];
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,11 +1,39 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { PostUserDto } from './dto/user.dto';
 import { UserService } from './user.service';
+import { ApiResponse, ApiBody, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  @ApiBody({
+    description: 'Request Body',
+    type: PostUserDto,
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'To Save User uuid',
+    content: {
+      'application/json': {
+        examples: {
+          uuid: {
+            value: {
+              fcmTokenId: 'fcmtoken',
+              createdAt: '2024-05-11T18:02:38.257Z',
+              lastCheckAt: '2024-05-11T18:02:38.257Z',
+              successCount: 0,
+              failCount: 0,
+              errors: null,
+              userUuid: '123e4567-e89b-12d3-a456-426614174000',
+              serviceId: 1,
+            },
+          },
+        },
+      },
+    },
+  })
   @Post()
   async PostUser(@Body() dataDto: PostUserDto): Promise<any> {
     return await this.userService.postuser(dataDto);
